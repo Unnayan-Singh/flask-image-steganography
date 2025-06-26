51c3d97f0479740d32e08585ca8e94e9ceb239c6
Unnayan-Singh <155305726+Unnayan-Singh@users.noreply.github.com>
2025-06-27 03:00:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -220,8 +220,7 @@ def logout():
     session.clear()
     return redirect('/login')
 
-if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=False, host="0.0.0.0", port=port)
+if __name__ == '__main__':
+    app.run(debug=True)
+    
 
