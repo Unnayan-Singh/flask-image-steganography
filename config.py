e0a171db2c04e8ad11c9851252fb69c726bc5886
Unnayan-Singh <155305726+Unnayan-Singh@users.noreply.github.com>
2025-06-27 12:40:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # config.py
-SECRET_KEY = 'xjd92!@asd_9283$#jfLWKDl@#1lwjf@31!'
+SECRET_KEY = 'your_secret_key_here'
 MYSQL_HOST = 'localhost'
 MYSQL_USER = 'root'
-MYSQL_PASSWORD = '1234'
-MYSQL_DB = 'steganography_app'
+MYSQL_PASSWORD = 'you_sql_password'
+MYSQL_DB = 'your_database_name'
